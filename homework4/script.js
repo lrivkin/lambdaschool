1d986281b947d23289aac9360f94f37fdefc2b8d
Leah Rivkin <lrivkin@wustl.edu>
2017-09-03 18:20:07 -0500

    finished homework 4

--- homework4/script.js
+++ homework4/script.js
@@ -1,1 +1,27 @@
 document.body.style.backgroundColor = "red";
+document.getElementById("aboutme").style.backgroundColor = "blue";
+document.body.style.fontFamily = "sans-serif";
+var temp = document.createTextNode("Leah");
+var selector = document.getElementById("nickname");
+selector.appendChild(temp);
+
+var temp = document.createTextNode("dogs, cookies");
+document.getElementById("favorites").appendChild(temp);
+
+var temp = document.createTextNode("Chicago");
+document.getElementById("hometown").appendChild(temp);
+
+var c = document.getElementsByTagName('li');
+var i;
+for (i = 0; i < c.length; i++) {
+  c[i].className += " listitem";
+}
+var c = document.getElementsByClassName("listitem");
+for (i=0; i< c.length; i++) {
+  c[i].style.color = "green";
+}
+
+var x = document.createElement("img");
+x.src= 'doggie.jpg';
+x.alt = "Dog";
+document.body.appendChild(x);
